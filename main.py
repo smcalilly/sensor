c19db872701a93a76baae0d64222c91a05f52c43
sam <smcalilly@gmail.com>
2020-05-10 22:05:14 -0500

    m

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ TEMP_CHANNEL = 1
 
 soil_readings = []
 temp_readings = []
+start_time = time.time()
 stop_time = time.time() + 5
 
-while time.time() < stop_time:
-    soil_reading = Sensor(SOIL_CHANNEL)
-    soil_readings.append(soil_reading.percentage)
-    
+while start_time < stop_time:
+    try:
+        soil_reading = Sensor(SOIL_CHANNEL)
+        soil_readings.append(soil_reading.percentage)
+    except Exception as e:
+        print(e)
+
 print(len(soil_readings))
 mean = numpy.mean(soil_readings)
 std = numpy.std(soil_readings)
@@ -22,3 +26,8 @@ print(mean)
 print(std)
 print(min)
 print(max)
+
+file = open('readings.csv', 'a')
+line = mean + "," + std + "," + min + "," + max + "," + stop_time
+file.write(line)
+file.close()
